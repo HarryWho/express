9b8909301331066546628f53806ae0e8137f2f59
HarryWho <37016939+HarryWho@users.noreply.github.com>
2018-03-08 04:25:16 +1100

    Added the express-handlebars modules

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var express=require('express');
+const express=require('express');
+const app=express();
+const handlebars = require('express-handlebars').create({defaultLayout:'main'})
 
-var app=express();
-
+app.use(express.static(__dirname+'/Public'));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.listen('5001', function(){
     console.log("Server listening on port 5001");
