577fee82418c77499e01df32a30cc4fe10d1bfd1
HarryWho <37016939+HarryWho@users.noreply.github.com>
2018-03-08 04:43:50 +1100

    Added routes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const app=express();
 const handlebars = require('express-handlebars').create({defaultLayout:'main'})
+const index = require('./routes/index');
 
-app.use(express.static(__dirname+'/Public'));
+app.use(express.static(__dirname+'/public'));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
+
 
 app.listen('5001', function(){
     console.log("Server listening on port 5001");
