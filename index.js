448188a0f084e39043e7b0efa2f7c8133be0bb4a
HarryWho <37016939+HarryWho@users.noreply.github.com>
2018-03-08 04:48:39 +1100

    Fixed the route index file

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const handlebars = require('express-handlebars').create({defaultLayout:'main'})
 const index = require('./routes/index');
 
 app.use(express.static(__dirname+'/public'));
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
+app.use('/', index);
 
 
 app.listen('5001', function(){
