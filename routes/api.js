a304e625d9c30848e8024cc034e0486f131eb84f
HarryWho <37016939+HarryWho@users.noreply.github.com>
2018-03-08 17:56:47 +1100

    Added the routes

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+
+const express= require('express');
+const api=express.Router();
+
+// get request for a list of ninjas
+api.get('/ninjas', (req, res)=>{
+    res.send({type: 'GET'});
+  });
+  
+  //post request to add a ninja
+api.post('/ninjas', (req, res)=>{
+    res.send({type: 'POST'});
+  });
+  
+  // put request to update a ninja
+api.put('/ninjas/:id', (req, res)=>{
+    res.send({type: 'PUT'});
+  });
+  
+  // delete request to delete a ninja
+api.delete('/ninjas/:id', (req, res)=>{
+    res.send({type: 'DELETE'});
+  });
+module.exports = api;
