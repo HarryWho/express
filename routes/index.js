9e8b913e10134dfe3ed73018f381d68f8b5b7d34
HarryWho <37016939+HarryWho@users.noreply.github.com>
2018-03-08 12:35:54 +1100

    Added AdminLTE styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('Calvins Express');
+    res.render('home',()=>{});
   });
 
   module.exports = router;
